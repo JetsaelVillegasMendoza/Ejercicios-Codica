668620e22133e372338e918666c5b9a1e56a5332
jvm <jetsaelvm@gmail.com>
2024-03-03 14:04:42 -0600

    Se realizaron los ejercicios 6 a 12.

--- js/main.js
+++ js/main.js
@@ -14,8 +14,53 @@ console.log('Renly');
 
 //Ejercicio 4
 
-console.log(9780262531962)
+console.log(9780262531962);
 
 //Ejercicio 5
 
 console.log('What Is Dead May Never Die.');
+
+//Ejercicio 6
+
+let x = 81 / 9; //=> 9
+
+console.log(x);
+
+//Ejercicio 7
+
+x = 6 - (-81);
+
+console.log(x);
+
+//Ejercicio 8
+
+x = 3 ** 5; //=> 243
+let y = (-8) / (-4); //=> 2
+
+console.log(x);
+console.log(y);
+
+//Ejercicio 9
+
+y = ((8 / 2) + 5) - (-3 / 2); //=> 10.5
+
+console.log(y);
+
+//Ejercicio 10
+
+x = 70 * 3 + 4 / 8 + 2; //=> 49
+x = 70 * (3 + 4) / (8 + 2); //Colocación de paréntesis para indicar qué operaciones se realizarán primero
+
+console.log(x)
+
+//Ejercicio 11
+
+x = 0.39 * 0.22;
+
+console.log(x);
+
+//Ejercicio 12
+
+x = (Infinity + Infinity) / 10;
+
+console.log(x);
