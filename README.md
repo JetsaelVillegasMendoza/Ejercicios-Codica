3fe32101f5ea41717009e050f6fafd358e72523a
jvm <jetsaelvm@gmail.com>
2024-03-06 21:13:46 -0600

    Se realizarón los ejercicios del 30 al 33 y se editó el archivoREADME.md

--- README.md
+++ README.md
@@ -256,3 +256,29 @@ Do you want to eat, Arya?
 ## Ejercicio 29
 
 Muestra en la pantalla el último carácter de la cadena que se encuentra en la constante "name".
+
+## Ejercicio 30
+
+Muestra en pantalla el número -0.304.
+
+## Ejercicio 31
+
+Muestra en la pantalla el valor undefined sin especificarlo explícitamente (sin asignarlo o pasarlo directamente a console.log()). Si no puedes resolverlo, mira la solución del profesor.
+
+## Ejercicio 32
+
+Se te han dado tres constantes con los apellidos de diferentes personas. Crea y muestra en pantalla una sola palabra con los caracteres en el siguiente orden:
+
+     1. El tercer carácter de la primera cadena de texto.
+     2. El segundo carácter de la segunda cadena de texto.
+     3. El cuarto carácter de la tercera cadena de texto.
+     4. El quinto carácter de la segunda cadena de texto.
+     5. El tercer carácter de la segunda cadena de texto.
+
+Intenta utilizar interpolación: dentro de las llaves puedes colocar no solamente variables enteras, sino también caracteres individuales utilizando corchetes.
+
+## Ejercicio 33
+
+Muestra en pantalla el resultado de la expresión: 7 - (-8 - -2). Intenta hacer que el número 7 sea una cadena de texto en lugar de un número. Experimenta también con otros números.
+
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //Ejercicio 1
-
 console.log('Hello, World')
 
 //Ejercicio 2
@@ -7,33 +6,27 @@ console.log('Hello, World')
     //You know nothing, Jon Snow!
 
 //Ejercicio 3
-
 console.log('Robert');
 console.log('Stannis');
 console.log('Renly');
 
 //Ejercicio 4
-
 console.log(9780262531962);
 
 //Ejercicio 5
-
 console.log('What Is Dead May Never Die.');
 
 //Ejercicio 6
-
 let x = 81 / 9; //=> 9
 
 console.log(x);
 
 //Ejercicio 7
-
 x = 6 - (-81);
 
 console.log(x);
 
 //Ejercicio 8
-
 x = 3 ** 5; //=> 243
 let y = (-8) / (-4); //=> 2
 
@@ -41,53 +34,44 @@ console.log(x);
 console.log(y);
 
 //Ejercicio 9
-
 y = ((8 / 2) + 5) - (-3 / 2); //=> 10.5
 
 console.log(y);
 
 //Ejercicio 10
-
 x = 70 * 3 + 4 / 8 + 2; //=> 49
 x = 70 * (3 + 4) / (8 + 2); //Colocación de paréntesis para indicar qué operaciones se realizarán primero
 
 console.log(x)
 
 //Ejercicio 11 //=> 0.0858
-
 x = 0.39 * 0.22;
 
 console.log(x);
 
 //Ejercicio 12
-
 x = (Infinity + Infinity) / 10; //=> Infinity
 
 console.log(x);
 
 //Ejercicio 13
-
 x = NaN + 1; //=> NaN
 
 console.log(x);
 
 //Ejercicio 14
     //"la diferencia entre el cuadrado de cinco y el producto de tres y siete"
-
 x = ((5**2) - (3*7)); 
 
 console.log(x);
 
 //Ejercicio 15
-
 console.log ("\"Khal Drogo's favorite word is \"athjahakar\"\"") //=> "Khal Drogo's favorite word is "athjahakar""
 
 //Ejercicio 16
-
 console.log('- Did Joffrey agree? \n- He did. He also said "I love using \\n".')
 
 //Ejercicio 17
-
 console.log("Winter " + "came " + "for " + "the " + "House " + "of " + "Frey" + ".")//=> Winter came for the House of Frey.
                                     
 //Ejercicio 18
@@ -167,3 +151,32 @@ console.log(`Do you want to eat, ${stark}?`);
 const name = 'Na\nharis';
 
 console.log(name[7]); // => s
+
+//Ejercicio 30
+let number = -0.304;
+
+console.log(number)
+
+//Ejercicio 31
+let sinEspecificar;
+
+console.log(sinEspecificar);
+
+//Ejercicio 32
+const one = 'Naharis';
+const two = 'Mormont';
+const three = 'Sand';
+
+let firstLetter = "h";
+let secondLetter = "o";
+let thirdLetter = "d";
+let fourthLetter = "o";
+let fiftLetter = "r";
+
+console.log(`${firstLetter}${secondLetter}${thirdLetter}${fourthLetter}${fiftLetter}`);
+
+//Ejercicio 33
+const exp = "7" - ((-8) - (-2)); //=> 13 Porque el operaddor "-" convierte la cadena de texto a un número. 
+
+console.log (exp);
+
